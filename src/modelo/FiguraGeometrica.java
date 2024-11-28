8603275755db2409f16f708a4146978fe636c3f8
falcuir <maria.alcusir1989@ist17dejulio.edu.ec>
2024-11-28 16:04:06 -0500

    Implementacion de la Interfaz

---
+++ src/modelo/FiguraGeometrica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Dell Core i7
+ */
+public interface FiguraGeometrica {
+    
+    //Declarar constantes --> final
+    //todo lo de interafaz va a ser publico
+    
+    public final double PI=3.1416;
+    
+    
+    // Declarar métodos sin implementarlos odeclarados
+    public void area();
+    public double perimetro();
+    
+}
